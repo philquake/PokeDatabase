6c87180efa29a184a161ad4fb2b5d5b294dd0c14
philquake <phillipcole123@gmail.com>
2023-06-13 19:04:45 -0500

    gen search troubleshoot

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pokebase as pb
-import logging
+import logging, pprint
 
 app = Flask(__name__)
 
@@ -9,6 +9,11 @@ class PokeObj:
         self.name = name
         self.id = id
         self.height = height
+
+class GenObj:
+    def __init__(self,name, gen):
+        self.name = name
+        self.gen = gen
 
 @app.route('/')
 def index():
@@ -31,14 +36,34 @@ def poke_detail(poke_name):
         id = poke.id
         height = poke.height
         pname = poke.name
-        app.logger.info(poke.types)
-        types = poke.types
+        # app.logger.info('Type',(poke.type_))
+        
+        # types = poke.types
         obj = PokeObj(pname,id,height)
-        return render_template('poke_detail.html', obj=obj, types = types)
-    except:
+        return render_template('poke_detail.html', obj=obj)
+    except: #Will handle Attribute Error if an incorrect pokemon name is entered
         error = "Please check your spelling, no such pokemon."
         return render_template('index.html',error=error)
+    
+@app.route('/gen', methods=['POST'])
+def gen():
+    if request.method == "POST":
+        gen = request.form['gen']
+        return redirect(url_for('generation', gen = gen))
+    return render_template('index.html')
 
+@app.route('/generation/<gen>', methods=['GET', 'POST'])
+def generation(gen):
+    error = None
+    # try:
+    gen_search = pb.generation(gen)
+    for pokemon in gen_search.pokemon_species:
+        obj = GenObj[pokemon] = pokemon.name.title()
+        app.logger.info('Type',(pokemon.name.title()))
+    return render_template('generation.html', obj=obj)
+    # except:
+    #     error = "No such generation."
+    #     return render_template('index.html',error=error)
 
 if __name__ =="__main__":
     app.run(debug=True)
