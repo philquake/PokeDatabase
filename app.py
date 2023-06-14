72f0a500f2d9486ffb12eba4f1b5612f58f9ac75
philquake <phillipcole123@gmail.com>
2023-06-14 16:43:45 -0500

    generation poke list functional

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ def poke_detail(poke_name):
         height = poke.height
         pname = poke.name
         # app.logger.info('Type',(poke.type_))
-        
         # types = poke.types
         obj = PokeObj(pname,id,height)
         return render_template('poke_detail.html', obj=obj)
@@ -55,25 +54,35 @@ def gen():
 @app.route('/generation/<gen>', methods=['GET', 'POST'])
 def generation(gen):
     error = None
-    list = []
-    # try:
-    match gen:
-        case "1":
-            GENERATION = 1
-        case "2":
-            GENERATION = 2
-        case _:
-            print("asad")
-    gen_resource = pb.generation(GENERATION)     # Get API data associated with that particular generation.
-    for pokemon in gen_resource.pokemon_species:     # Iterate through the list of Pokemon introduced in that generation.
-        # print(pokemon.name.title())
-        list.append(GenObj(GENERATION,pokemon.name.title()))
-    for x in list:
-        print(GenObj[x])
-    return render_template('generation.html', GenObj=GenObj)
-    # except:
-    #     error = "No such generation."
-    #     return render_template('index.html',error=error)
+    genlist = []
+    try:
+        match gen:
+            case "1":
+                GENERATION = 1
+            case "2":
+                GENERATION = 2
+            case "3":
+                GENERATION = 3
+            case "4":
+                GENERATION = 4
+            case "5":
+                GENERATION = 5
+            case "6":
+                GENERATION = 6
+            case "7":
+                GENERATION = 7
+            case "8":
+                GENERATION = 8
+            case "9":
+                GENERATION = 9
+            case other:
+                error = "No such generation."
+        gen_resource = pb.generation(GENERATION)     # Get API data associated with that particular generation.
+        for pokemon in gen_resource.pokemon_species:     # Iterate through the list of Pokemon introduced in that generation.
+            genlist.append(GenObj(GENERATION,pokemon.name.title()))
+        return render_template('generation.html', genlist = genlist, len = len(genlist))
+    except:
+        return render_template('index.html',error=error)
 
 if __name__ =="__main__":
     app.run(debug=True)
