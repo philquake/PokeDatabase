f883780f4a0cdb947200e880e0fa5d2b7b3084d7
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-05 14:59:18 -0500

    Reframe and completed functional_test for homepage

--- functional_test/test_home.py
+++ functional_test/test_home.py
@@ -1,9 +1,15 @@
-from django.test import LiveServerTestCase
+from django.test import LiveServerTestCase, SimpleTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class NewUserTest(LiveServerTestCase):
+class HomePageTest(SimpleTestCase):
+    def test_home_page_returns_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "base.html")
+
+class TemplateTest(LiveServerTestCase):
     def setUp(self):
         super().setUp()
         self.driver = webdriver.Chrome()
@@ -13,14 +19,38 @@ class NewUserTest(LiveServerTestCase):
         super().tearDown()
 
     def test_correct_template(self):
-        self.driver.get(self.live_server_url + "/")
+        self.driver.get(self.live_server_url)
         self.assertIn("Home", self.driver.title)
         header_text = self.driver.find_element(By.TAG_NAME, "h1").text
         self.assertIn("Search Pokemon", header_text)
 
-    def navigation_bar_links(self):
+class NavigationBarTest(LiveServerTestCase):
+    def setUp(self):
+        super().setUp()
+        self.driver = webdriver.Chrome()
+
+    def tearDown(self):
+        self.driver.quit()
+        super().tearDown()
+
+    def test_navigation_bar_links(self):
         self.driver.get(self.live_server_url + "/")
         nav_links = self.driver.find_elements(By.CSS_SELECTOR, "nav a")
         link_texts = [link.text for link in nav_links]
-        expected_links = ["Home", "Pokemon", "Types", "Abilities", "Moves", "Regions"]
+        expected_links = ["Home", "Pokédex", "Types", "Abilities", "Moves", "Regions"]
         self.assertEqual(link_texts, expected_links)
+
+
+class searchBarTest(LiveServerTestCase):
+    def setUp(self):
+        super().setUp()
+        self.driver = webdriver.Chrome()
+
+    def tearDown(self):
+        self.driver.quit()
+        super().tearDown()
+
+    def test_search_bar_present(self):
+        self.driver.get(self.live_server_url + "/")
+        search_bar = self.driver.find_element(By.CSS_SELECTOR, "form[method='GET']")
+        self.assertIsNotNone(search_bar)
