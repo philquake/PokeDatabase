4c1d67eaa3ba79cd8a894dac75039dcf29086532
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-05 18:28:48 -0500

    Add comment to clarify inclusion of home app in installed apps

--- PokeDatabase/settings.py
+++ PokeDatabase/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
+    'home',  # Add the home app to the installed apps
 ]
 
 MIDDLEWARE = [
