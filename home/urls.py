fb8b8235ca7697997e3b090c8b6c0ffd0d92b7f4
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-05 23:12:25 -0500

    Refactor URL patterns to include additional routes for pokedex, types, region, abilities, moves, and competitive

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("pokedex/", views.pokedex, name="pokedex"),
+    path("types/", views.types, name="types"),
+    path("region/", views.region, name="region"),
+    path("abilities/", views.abilities, name="abilities"),
+    path("moves/", views.moves, name="moves"),
+    path("competitive/", views.competitive, name="competitive"),
 ]
