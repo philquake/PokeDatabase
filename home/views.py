4d5ab792f63c110f41dfe66944bfd43bc3214978
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-06 09:59:21 -0500

    dummy json passed for testing

--- home/views.py
+++ home/views.py
@@ -1,10 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import json
+from pathlib import Path
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+
+    pokemon = {
+        "name": "Charizard",
+        "pokedex_number": 6,
+        "types": ["Fire", "Flying"],
+        "image": {
+            "official_artwork": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
+        },
+        "stats": {
+            "highest": [
+                {"name": "Special Attack", "value": 109},
+                {"name": "Speed", "value": 100},
+            ]
+        },
+    }
+
+    return render(request, "home.html", {"pokemon": pokemon})
 
 def search(request):
     return render(request, 'search.html')
