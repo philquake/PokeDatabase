e3f71d599366038f1bade803977f25e9801df3fb
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-07 18:05:35 -0500

    reorder url fixed test_search redirect bug

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -4,13 +4,13 @@ from pokemon import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("search/", views.search, name="search"),
     path("<str:name>/", views.pokemon_detail, name="pokemon_detail"),
-
     path("pokedex/", views.pokedex, name="pokedex"),
     path("types/", views.types, name="types"),
     path("region/", views.region, name="region"),
     path("abilities/", views.abilities, name="abilities"),
     path("moves/", views.moves, name="moves"),
     path("competitive/", views.competitive, name="competitive"),
-    path("search/", views.search, name="search"),
+    
 ]
