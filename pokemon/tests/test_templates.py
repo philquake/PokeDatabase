56e38b721e0f0664a8e51a47f279ac00c5b8f154
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-06 22:42:57 -0500

    test_templates in one file

---
+++ pokemon/tests/test_templates.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+import lxml.html
+
+# Create your tests here.
+class HomePageTest(TestCase):
+    def test_home_page_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class PokemonDetailPageTest(TestCase):
+    def test_pokemon_detail_page_template(self):
+        response = self.client.get("/pokemon/94/")
+        self.assertTemplateUsed(response, 'pokemon_detail.html')
+        
