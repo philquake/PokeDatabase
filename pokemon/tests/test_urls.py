808821b237a78d3f8d6c3ce021646439ae601baf
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-07 10:34:18 -0500

    new test for urls added

---
+++ pokemon/tests/test_urls.py
@@ -0,0 +1,23 @@
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+
+from pokemon import views
+
+
+class PokemonURLTests(SimpleTestCase):
+
+    def test_pokemon_detail_url_resolves(self):
+        resolver = resolve("/pokemon/Gengar/")
+        self.assertEqual(resolver.func, views.pokemon_detail)
+
+    def test_pokemon_detail_url_name(self):
+        resolver = resolve("/pokemon/Gengar/")
+        self.assertEqual(resolver.url_name,"pokemon_detail")
+
+    def test_pokemon_detail_captures_name(self):
+        resolver = resolve("/pokemon/Gengar/")
+        self.assertEqual(resolver.kwargs["name"],"Gengar")
+
+    def test_pokemon_detail_reverse(self):
+        url = reverse("pokemon_detail",kwargs={"name": "Gengar"})
+        self.assertEqual(url,"/pokemon/Gengar/")
