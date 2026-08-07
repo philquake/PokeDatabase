21551e4397e14a03fef0aaa071bc8caf4db0b93a
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-06 22:44:55 -0500

    simplify views

--- pokemon/views.py
+++ pokemon/views.py
@@ -23,125 +23,28 @@ def home(request):
         },
     }
 
-    multiple_featured = [
-        {
-            "pokedex": 6,
-            "name": "Charizard",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
-            "types": ["Fire", "Flying"],
-            "top_stats": {
-                "Special Attack": 109,
-                "Speed": 100
-            }
-        },
-        {
-            "pokedex": 94,
-            "name": "Gengar",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png",
-            "types": ["Ghost", "Poison"],
-            "top_stats": {
-                "Special Attack": 130,
-                "Speed": 110
-            }
-        },
-        {
-            "pokedex": 130,
-            "name": "Gyarados",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/130.png",
-            "types": ["Water", "Flying"],
-            "top_stats": {
-                "Attack": 125,
-                "Special Defense": 100
-            }
-        },
-        {
-            "pokedex": 149,
-            "name": "Dragonite",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/149.png",
-            "types": ["Dragon", "Flying"],
-            "top_stats": {
-                "Attack": 134,
-                "HP": 91
-            }
-        },
-        {
-            "pokedex": 212,
-            "name": "Scizor",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/212.png",
-            "types": ["Bug", "Steel"],
-            "top_stats": {
-                "Attack": 130,
-                "Defense": 100
-            }
-        },
-        {
-            "pokedex": 248,
-            "name": "Tyranitar",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/248.png",
-            "types": ["Rock", "Dark"],
-            "top_stats": {
-                "Attack": 134,
-                "Defense": 110
-            }
-        },
-        {
-            "pokedex": 472,
-            "name": "Gliscor",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/472.png",
-            "types": ["Ground", "Flying"],
-            "top_stats": {
-                "Defense": 125,
-                "Speed": 95
-            }
-        },
-        {
-            "pokedex": 448,
-            "name": "Lucario",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/448.png",
-            "types": ["Fighting", "Steel"],
-            "top_stats": {
-                "Special Attack": 115,
-                "Attack": 110
-            }
-        },
-        {
-            "pokedex": 445,
-            "name": "Garchomp",
-            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/445.png",
-            "types": ["Dragon", "Ground"],
-            "top_stats": {
-                "Attack": 130,
-                "Speed": 102
-            }
-        }
-    ]
-    
-    return render(request, "home.html", {"pokemon": featured, "multiple_featured": multiple_featured})
+    return render(request, "home.html", {"pokemon": featured})
+
+def pokemon_detail(request, pokedex_number):
+    return render(request, 'pokemon_detail.html', {"pokedex_number": pokedex_number})
 
 def search(request):
-    return render(request, 'search.html')
-
-
-def pokedex(request):
-    return HttpResponse("Pokédex")
-
+    return HttpResponse("Search functionality is not implemented yet.")
 
 def types(request):
-    return HttpResponse("Types")
+    return HttpResponse("Types page is not implemented yet.")
 
+def pokedex(request):
+    return HttpResponse("Pokedex page is not implemented yet.")
 
 def region(request):
-    return HttpResponse("Region")
-
+    return HttpResponse("Region page is not implemented yet.")
 
 def abilities(request):
-    return HttpResponse("Abilities")
-
+    return HttpResponse("Abilities page is not implemented yet.")
 
 def moves(request):
-    return HttpResponse("Moves")
-
+    return HttpResponse("Moves page is not implemented yet.")
 
 def competitive(request):
-    return HttpResponse("Competitive")
-    
+    return HttpResponse("Competitive Analysis page is not implemented yet.")
