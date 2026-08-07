d81a287b91d04356626d1f8dec1825e0b1a8862e
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-06 22:44:30 -0500

    added default views again

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
-from . import views
+from pokemon import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("search/", views.search, name="search"),
+    path("<int:pokedex_number>/", views.pokemon_detail, name="pokemon_detail"),
+
     path("pokedex/", views.pokedex, name="pokedex"),
     path("types/", views.types, name="types"),
     path("region/", views.region, name="region"),
     path("abilities/", views.abilities, name="abilities"),
     path("moves/", views.moves, name="moves"),
     path("competitive/", views.competitive, name="competitive"),
+    path("search/", views.search, name="search"),
 ]
