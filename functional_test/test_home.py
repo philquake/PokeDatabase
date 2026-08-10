cc16d24d0018d5fcd8baceee82499f63412dcf94
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-09 19:23:39 -0500

    test: moved the appropriate test to the correct document

--- functional_test/test_home.py
+++ functional_test/test_home.py
@@ -1,69 +1,34 @@
-from django.test import LiveServerTestCase, SimpleTestCase
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class HomePageTest(SimpleTestCase):
+class HomePageTest(LiveServerTestCase):
+    
+    def setUp(self):
+            super().setUp()
+            self.driver = webdriver.Chrome()
+    
+    def tearDown(self):
+        self.driver.quit()
+        super().tearDown()
+            
     def test_home_page_returns_200(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "base.html")
-
-class TemplateTest(LiveServerTestCase):
-    def setUp(self):
-        super().setUp()
-        self.driver = webdriver.Chrome()
-
-    def tearDown(self):
-        self.driver.quit()
-        super().tearDown()
-
-    def test_correct_template(self):
-        self.driver.get(self.live_server_url)
-        self.assertIn("Home", self.driver.title)
-        paragraph = self.driver.find_element(By.CLASS_NAME, "hero-subtitle")
-        self.assertIn("Search Pokémon", paragraph.text)
-
-class NavigationBarTest(LiveServerTestCase):
-    def setUp(self):
-        super().setUp()
-        self.driver = webdriver.Chrome()
-
-    def tearDown(self):
-        self.driver.quit()
-        super().tearDown()
-
-    def test_navigation_bar_links(self):
-        self.driver.get(self.live_server_url + "/")
-        nav_links = self.driver.find_elements(By.CSS_SELECTOR, "nav a")
-        link_texts = [link.text for link in nav_links]
-        expected_links = ["PokeDatabase", "Home", "Pokédex", "Types", "Region", "Abilities", "Moves", "Competitive"]
-        self.assertEqual(link_texts, expected_links)
-
-class searchBarTest(LiveServerTestCase):
-    def setUp(self):
-        super().setUp()
-        self.driver = webdriver.Chrome()
-
-    def tearDown(self):
-        self.driver.quit()
-        super().tearDown()
-
+    
     def test_search_bar_present(self):
         self.driver.get(self.live_server_url + "/")
         search_bar = self.driver.find_element(By.CSS_SELECTOR, "form[method='GET']")
         self.assertIsNotNone(search_bar)
-
-class HomeLinkActive(LiveServerTestCase):
-    def setUp(self):
-        super().setUp()
-        self.driver = webdriver.Chrome()
         
-    def tearDown(self):
-        self.driver.quit()
-        super().tearDown()
-        
-    def test_active_hmtl(self):
+    def test_hero_pokemon(self):
         self.driver.get(self.live_server_url + "/")
-        link = self.driver.find_element(By.CSS_SELECTOR, "a")
-        assert link.get_attribute("class").find("active") != -1
+        image = self.driver.find_element(By.CLASS_NAME, "pokemon-image")
+        self.assertEqual(image.is_displayed(), True)
+        
+    # def test_featured_pokemon(self):
+    #     self.driver.get(self.live_server_url + "/")
+    #     image = self.driver.find_element(By.CLASS_NAME, "poke-card-img")
+    #     self.assertEqual(image.is_displayed(), True)
--- functional_test/test_navigation.py
+++ functional_test/test_navigation.py
@@ -9,6 +9,13 @@ class Navigation_Links(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+        
+    def test_navigation_bar_links(self):
+        self.driver.get(self.live_server_url + "/")
+        nav_links = self.driver.find_elements(By.CSS_SELECTOR, "nav a")
+        link_texts = [link.text for link in nav_links]
+        expected_links = ["PokeDatabase", "Home", "Pokédex", "Types", "Region", "Abilities", "Moves", "Competitive"]
+        self.assertEqual(link_texts, expected_links)
 
     def test_navigation_links_redirect(self):
 
@@ -26,6 +33,12 @@ class Navigation_Links(LiveServerTestCase):
 
         self.assertEqual(self.browser.current_url, self.live_server_url + "/pokemon/")
 
+    def test_active_hmtl(self):
+        self.driver.get(self.live_server_url + "/")
+        link = self.driver.find_element(By.CSS_SELECTOR, "a.home")
+        assert "active" in link.get_attribute("class")
+    
+    
     # def test_404_page_navigation_works(self):   ##remember DEBUG = FALSE if testing, need to fix
     #     # Navigate to a page that does not exist
     #     self.browser.get(self.live_server_url + "/does-not-exist/")
