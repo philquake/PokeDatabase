64b4f0129634a3a05f69c305449d6815d14fc801
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-11 17:09:37 -0500

    fix: missing test

--- pokemon/tests/test_views.py
+++ pokemon/tests/test_views.py
@@ -32,5 +32,61 @@ class PokemonViewTest(SimpleTestCase):
 
         self.assertRedirects(
             response,
-          "/pokemon/pokedex/Gengar/"
-      )
+            reverse("pokemon_detail", args=["Gengar"])
+        )
+
+    def test_search_result_contains_gengar(self):
+        response = self.client.get(
+            reverse("search"),
+            {"search": "Gengar"},
+            follow=True
+        )
+
+        self.assertContains(response, "Gengar")
+    
+    def test_home_context_contains_required_data(self):
+        response = self.client.get(reverse("home"))
+        
+        self.assertEqual(response.status_code, 200)
+
+        required_keys = [
+            "pokemon_count",
+            # "move_count",
+            # "ability_count",   ###TO FIX AFTER DATABASE API IS LINKED
+            "explore_pokemon",
+            "hero",
+            "featured_pokemon",
+        ]
+
+        for key in required_keys:
+            self.assertIn(key, response.context)
+            
+    def test_home_context_has_correct_types(self):
+        response = self.client.get(reverse("home"))
+        context = response.context
+
+        self.assertIsInstance(context["pokemon_count"], int)
+        # self.assertIsInstance(context["move_count"], int)
+        # self.assertIsInstance(context["ability_count"], int)
+        self.assertIsInstance(context["explore_pokemon"], str)
+        self.assertIsInstance(context["hero"], dict)
+        self.assertIsInstance(context["featured_pokemon"], list)
+        
+    def test_home_context_has_data(self):
+        response = self.client.get(reverse("home"))
+        context = response.context
+
+        self.assertIsNotNone(context["hero"])
+
+        self.assertTrue(context["featured_pokemon"])
+        self.assertTrue(context["explore_pokemon"])
+        self.assertTrue(context["pokemon_count"])
+        
+    def test_home_has_correct_number_of_featured_pokemon(self):
+        response = self.client.get(reverse("home"))
+        context = response.context
+
+        self.assertEqual(
+            len(context["featured_pokemon"]),
+            settings.FEATURED_POKEMON_COUNT
+        )
