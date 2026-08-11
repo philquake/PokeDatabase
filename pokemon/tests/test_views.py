95dc935423c4d27dec768c29d07d0a88976e4c52
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-08-11 16:54:10 -0500

    test: updated unit test

--- pokemon/tests/test_views.py
+++ pokemon/tests/test_views.py
@@ -1,9 +1,11 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
+from django.conf import settings
 
 class PokemonViewTest(SimpleTestCase):
 
     def test_pokemon_detail_url_resolves_to_correct_view(self):
-     response = self.client.get("/pokemon/pokedex/Gengar/")
+     response = self.client.get(reverse("pokemon_detail" , args=["Gengar"]))
 
      self.assertTemplateUsed(
         response,
@@ -11,17 +13,24 @@ class PokemonViewTest(SimpleTestCase):
      )
 
     def test_pokemon_detail_passes_pokemon_to_template(self):
-        response = self.client.get("/pokemon/pokedex/Gengar/")
-
+        response = self.client.get(reverse("pokemon_detail" , args=["Gengar"]))
         pokemon = response.context["pokemon"]
 
         self.assertEqual(pokemon["name"], "Gengar")
         self.assertEqual(pokemon["types"], ["Ghost", "Poison"])
 
-    def test_search_redirects_to_pokemon_detail(self):
-      response = self.client.get("/pokemon/search/?search=Gengar")
-      
-      self.assertRedirects(
-          response,
+    def test_home_view_returns_200(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        
+
+    def test_search_view_returns_302(self):
+        response = self.client.get(
+            reverse("search"),
+            {"search": "Gengar"}
+        )
+
+        self.assertRedirects(
+            response,
           "/pokemon/pokedex/Gengar/"
       )
